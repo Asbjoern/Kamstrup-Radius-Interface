81b28b87fe1f64287fada7d3cf78d60e1670a2a7
Asbjørn Mejnertsen <asbjoern@mejnertsen.dk>
2020-01-24 08:02:36 +0100

    Added initial wifimanager configuration.
    Not happy about current configuration not loaded to config page

--- kamstrup-radius-mqtt/kamstrup-radius-mqtt.ino
+++ kamstrup-radius-mqtt/kamstrup-radius-mqtt.ino
@@ -1,10 +1,39 @@
+#include <FS.h>  
+#include <DoubleResetDetect.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <Ticker.h>
+#include <ArduinoJson.h>
+
 #include "gcm.h"
 #include "mbusparser.h"
 
+#define DRD_TIMEOUT 5.0
+#define DRD_ADDRESS 0x00
+
 const size_t headersize = 11;
 const size_t footersize = 3;
-const uint8_t encryption_key[] =     {0x5A,0xD8,0x41,0x21,0xD9,0xD2,0x0B,0x36,0x4B,0x7A,0x11,0xF3,0xC1,0xB5,0x82,0x7F};
-const uint8_t authentication_key[] = {0xAF,0xB3,0xF9,0x3E,0x3E,0x72,0x04,0xED,0xB3,0xC2,0x7F,0x96,0xDB,0xD5,0x1A,0xE0};
+
+char mqtt_server[40];
+char mqtt_port[6] = "1883";
+char mqtt_uid[40];
+char mqtt_pwd[40];
+char hostname[40] ="KamstrupMQTT";
+char conf_key[] =     "5AD84121D9D20B364B7A11F3C1B5827F";
+char conf_authkey[] = "AFB3F93E3E7204EDB3C27F96DBD51AE0";
+
+uint8_t encryption_key[16];
+uint8_t authentication_key[16];
+
+bool shouldSaveConfig = false;
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
 uint8_t input[] = {0x7E,0xA1,0xE9,0x41,0x03,0x13,0xC6,0x37,0xE6,0xE7,0x00,
 0xDB,0x08,0x4B,0x41,0x4D,0x45,0x01,0xA4,0xDC,0x52,0x82,0x01,0xD0,0x30,0x00,0x07,0x88,0xE1,0xA0,0x39,0xB2,0xD1,0x4C,0x71,
 0x2D,0xD4,0xD8,0xC8,0x44,0x0D,0x53,0x68,0xE4,0x33,0xBD,0x70,0xB7,0x36,0x81,0xE9,0xA9,0xEF,0xFE,0x38,0xF1,0x75,0xA3,0x7D,0xE9,0xCD,0xE6,0x4E,0x8F,
@@ -25,22 +54,135 @@ uint8_t input[] = {0x7E,0xA1,0xE9,0x41,0x03,0x13,0xC6,0x37,0xE6,0xE7,0x00,
 0x1E,0x4E,0x68,0x4F,0x0B,0x26,0x85,0xC4,0xB6,0x9D,0x96,0xF5,0xFB,0x52,0xD0,0xB8,0x12,0x79,
 0x36,0x24,0x7E};
 
-uint8_t receiveBuffer[1000];
+uint8_t receiveBuffer[500];
 uint8_t decryptedFrameBuffer[500];
 VectorView decryptedFrame(decryptedFrameBuffer,0);
 
+DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
 mbedtls_gcm_context m_ctx;
 
 void setup() {
   Serial.begin(115200);
+  Serial.println();
+  Serial.println("mounting FS...");
 
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        StaticJsonDocument<512> doc;
+        DeserializationError error = deserializeJson(doc,configFile);
+        if (error){
+          Serial.println(F("Failed to read file, using default configuration"));
+          Serial.println(error.c_str());
+        }
+        else
+        {
+          strcpy(mqtt_server, doc["mqtt_server"]);
+          strcpy(mqtt_port, doc["mqtt_port"]);
+          strcpy(mqtt_uid, doc["mqtt_uid"]);
+          strcpy(mqtt_pwd, doc["mqtt_pwd"]);
+          strcpy(hostname, doc["hostname"]);
+          strcpy(conf_key, doc["conf_key"]);
+          strcpy(conf_authkey, doc["conf_authkey"]);
+        }
+        configFile.close();
+        serializeJson(doc, Serial);
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+  WiFiManagerParameter custom_mqtt_userid("userid", "mqtt uuid", mqtt_uid, 40);
+  WiFiManagerParameter custom_mqtt_pwd("password", "mqtt pwd", mqtt_pwd, 40);
+  WiFiManagerParameter custom_hostname("hostname", "hostname", hostname, 40);
+  WiFiManagerParameter custom_enckey("encryption key", "encryption key", conf_key, 16);
+  WiFiManagerParameter custom_authkey("authentication key", "authentication key", conf_authkey, 16);
+
+  WiFiManager wifiManager;
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_userid);
+  wifiManager.addParameter(&custom_mqtt_pwd);
+  wifiManager.addParameter(&custom_hostname);
+  wifiManager.addParameter(&custom_enckey);
+  wifiManager.addParameter(&custom_authkey);
+  
+  if (drd.detect())
+  {
+    Serial.println("** Double reset boot **");
+    if (!wifiManager.startConfigPortal(hostname)) {
+      Serial.println("failed to connect and hit timeout");
+    }
+  }
+  else
+  {
+    Serial.println("** Normal boot **");
+    if (!wifiManager.autoConnect(hostname)) {
+      Serial.println("failed to connect and hit timeout");
+    }
+  }
+
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_uid, custom_mqtt_userid.getValue());
+  strcpy(mqtt_pwd, custom_mqtt_pwd.getValue());
+  strcpy(hostname, custom_hostname.getValue());
+  strcpy(conf_key, custom_enckey.getValue());
+  strcpy(conf_authkey, custom_authkey.getValue());
+
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    StaticJsonDocument<512> json;
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["mqtt_uid"] = mqtt_uid;
+    json["mqtt_pwd"] = mqtt_pwd;
+    json["hostname"] = hostname;
+    json["conf_key"] = conf_key;
+    json["conf_authkey"] = conf_authkey;
+    serializeJson(json, Serial);
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+    serializeJson(json, configFile);
+    configFile.close();
+    ESP.reset();
+  }
+  
+
+  f(encryption_key, conf_key, sizeof(encryption_key));
+  f(authentication_key, conf_authkey, sizeof(authentication_key));
+  Serial.print("Encryptionkey: ");
+  printHex(encryption_key,sizeof(encryption_key));
+  Serial.println();
+  Serial.print("authentication_key: ");
+  printHex(authentication_key,sizeof(authentication_key));
+  Serial.println();
+
+  
+}
+
+void loop() {
+  return;
   MbusStreamParser streamParser(receiveBuffer, sizeof(receiveBuffer));
   for(int i=0;i<sizeof(input);i++){
     if (streamParser.pushData(input[i])) {
       VectorView frame = streamParser.getFrame();
       if (streamParser.getContentType() == MbusStreamParser::COMPLETE_FRAME) {
         Serial.println("Frame complete");
-        decrypt(frame);
+        if(!decrypt(frame))
+          Serial.println("Decryption failed");
         MeterData md = parseMbusFrame(decryptedFrame);
         if (md.activePowerPlusValid) {
           Serial.print("ActivePower: ");
@@ -60,14 +202,10 @@ void setup() {
         }
       } else {
         Serial.print(".");
-      //writeDebugStringToBuf(debugbuf, sizeof(debugbuf), MeterData(), frame);
       }
     }
   }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+  delay(10000);
 
 }
 
@@ -97,66 +235,52 @@ bool decrypt(const VectorView& frame){
   uint8_t initialization_vector[12];
   memcpy(initialization_vector,system_title,8);
   memcpy(initialization_vector+8,decryptedFrameBuffer+headersize+14,4);
-  Serial.print("initialization_vector:");
-  printHex(initialization_vector,sizeof(initialization_vector));
-  Serial.println();
   
   uint8_t additional_authenticated_data[17];
   memcpy(additional_authenticated_data,decryptedFrameBuffer+headersize+13,1);
   memcpy(additional_authenticated_data+1,authentication_key,16);
-  Serial.print("additional_authenticated_data:");
-  printHex(additional_authenticated_data,sizeof(additional_authenticated_data));
-  Serial.println();
   
   uint8_t authentication_tag[12];
   memcpy(authentication_tag,decryptedFrameBuffer+headersize+frame.size()-headersize-footersize-12,12);
-  Serial.print("authentication_tag:");
-  printHex(authentication_tag,sizeof(authentication_tag));
-  Serial.println();
-
+ 
   uint8_t cipher_text[frame.size()-headersize-footersize-18-12];
   memcpy(cipher_text,decryptedFrameBuffer+headersize+18,frame.size()-headersize-footersize-12-18);
-  Serial.print("cipher_text:");
-  printHex(cipher_text,sizeof(cipher_text));
-  Serial.println();
-
+ 
   uint8_t plaintext[sizeof(cipher_text)];
-   
-  if(decryptWrapper(sizeof(cipher_text),cipher_text,plaintext,sizeof(authentication_tag),authentication_tag,sizeof(initialization_vector),initialization_vector,
-      sizeof(additional_authenticated_data), additional_authenticated_data,sizeof(encryption_key)*8,encryption_key))
-      {
-        Serial.print("plaintext:");
-        printHex(plaintext,sizeof(plaintext));
-        Serial.println(); 
-      }
-   else
-    return false;
-  //copy replace encrypted data with decrypted for mbusparser library. Checksum not updated. Hopefully not needed
-  memcpy(decryptedFrameBuffer+headersize+18,plaintext,sizeof(plaintext));
-  decryptedFrame = VectorView(decryptedFrameBuffer,frame.size());
-  Serial.print("Decrypted input:");
-  printHex(decryptedFrame);
-  Serial.println(); 
-  return true;
-}
 
-bool decryptWrapper(const size_t length, const unsigned char* input, unsigned char* output,
-                      const size_t tag_length, unsigned char* tag, const size_t iv_length,
-                      unsigned char* iv, const size_t aad_length, const unsigned char* aad,
-                      const size_t key_length_bits,const unsigned char* key) {
   mbedtls_gcm_init(&m_ctx);
-  int success = mbedtls_gcm_setkey(&m_ctx, MBEDTLS_CIPHER_ID_AES, key, key_length_bits);
+  int success = mbedtls_gcm_setkey(&m_ctx, MBEDTLS_CIPHER_ID_AES, encryption_key, sizeof(encryption_key)*8);
   if (0 != success ) {
     Serial.print("Setkey failed: ");
     Serial.println(success);
     return false;
   }
-
-  success = mbedtls_gcm_auth_decrypt(&m_ctx, length, iv, iv_length, aad, aad_length, tag, tag_length, input, output);
+  success = mbedtls_gcm_auth_decrypt(&m_ctx, sizeof(cipher_text), initialization_vector, sizeof(initialization_vector), 
+    additional_authenticated_data, sizeof(additional_authenticated_data), authentication_tag, sizeof(authentication_tag), 
+    cipher_text, plaintext);
   if (0 != success) {
     Serial.print("authdecrypt failed: ");
     Serial.println(success);
     return false;
   }
   mbedtls_gcm_free(&m_ctx);
+    
+  //copy replace encrypted data with decrypted for mbusparser library. Checksum not updated. Hopefully not needed
+  memcpy(decryptedFrameBuffer+headersize+18,plaintext,sizeof(plaintext));
+  decryptedFrame = VectorView(decryptedFrameBuffer,frame.size());
+  
+  return true;
+}
+
+void f(uint8_t *dest, const char *source, int bytes_n)
+{
+    uint8_t *dst = dest;
+    uint8_t *end = dest + sizeof(bytes_n);
+    unsigned int u;
+
+    while (dest < end && sscanf(source, "%2x", &u) == 1)
+    {
+        *dst++ = u;
+        source += 2;
+    }
 }
